776d3123a72da46b6e50a78fd503d4449ed614bd
Niklas Garbe <ngarbe@hotmail.de>
2024-04-21 15:15:42 +0200

    Ambisonics Display now functional, added documentation

--- code/xypad.js
+++ code/xypad.js
@@ -1,7 +1,10 @@
 /*
-X/Y Pad for Surround Panning
+Modified version of "X/Y Pad for Surround Panning" by
 Nico Starke @ Ableton / Robert Henke
 Jan. 25 2018
+
+Adjusted for ReaperBridge usage by Niklas Garbe
+21.04.2023
 */
 
 outlets = 1;
@@ -17,6 +20,9 @@ var height = this.box.getattr("presentation_rect")[3];
 	
 var xVal = 0;
 var yVal = 0;
+var azimuth = 0;
+var elevation = 1;
+
 var xyCoords = 0;
 var focusSize = 0;
 var xyOffset = 20;
@@ -43,45 +49,8 @@ function paint()
 		// draw xy circle
 		set_source_rgba(xycolor);
 		ellipse(xVal-(xySize/2), yVal-(xySize/2), xySize, xySize);
-		if (mouseIsDown) 
-			fill();
-		else
-			set_line_width(2);
-			stroke();	
+		fill();	
 	}
-}
-
-
-function onclick(x, y, button, cmd, shift, capslock, option, ctrl){
-
-		mouseDownPos = [x, y];
-		mouseIsDown = button;
-
-		x = clamp(x, xyOffset, width-xyOffset);
-		y = clamp(y, xyOffset, height-xyOffset);
-		xyCoords = screentoworld(x, y);			
-		outlet(0, xyCoords);
-}
-
-
-function ondrag(x, y, button, cmd, shift, capslock, option, ctrl){
-
-		mouseIsDown = button;
-	
-		if (button){
-			x = clamp(x, xyOffset, width-xyOffset);
-			y = clamp(y, xyOffset, height-xyOffset);
-			xyCoords = screentoworld(x, y);
-		}
-
-		outlet(0, xyCoords);
-}
-
-
-function onidleout(x, y, button, cmd, shift, capslock, option, ctrl){
-
-        // reset circle fill when mouse is released outside of the jsui
-        mouseIsDown = button;   
 }
 
 // set x/y position
@@ -93,8 +62,49 @@ function setPos(x, y, focus){
 	mgraphics.redraw();
 }
 
-function setFocus(focus){
-	focusSize = focus
+// Takes input azimuth-degrees and calculates corresponding position on screen
+function setAzimuth(deg){
+	azimuth = degtorad(deg)
+
+	x = Math.sin(azimuth)
+	y = Math.cos(azimuth)
+
+	x = elevation * x
+	y = elevation * y
+
+	xVal = worldtoscreen(x, y)[0];
+	yVal = worldtoscreen(x, y)[1];
+
+	mgraphics.redraw();
+
+}
+
+// Takes input elevation-degrees and calculates corresponding position on screen
+function setElevation(deg){
+
+	rad = degtorad(deg)
+	elevation = Math.cos(rad)
+
+	x = Math.sin(azimuth)
+	y = Math.cos(azimuth)
+
+	x = elevation * x
+	y = elevation * y
+
+	xVal = worldtoscreen(x, y)[0];
+	yVal = worldtoscreen(x, y)[1];
+
+	xySize = 3 * Math.sin(rad) + 16
+
+	mgraphics.redraw();
+}
+
+// Takes input width (stereo spread)-degrees and displays them using an additional ellipse
+function setWidth(deg){
+	rad = degtorad(deg)
+
+	focusSize = 180 * Math.sin(0.5 * rad)
+
 	mgraphics.redraw();
 }
 
